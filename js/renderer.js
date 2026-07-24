e27a597debbae554b7c78bccc81d4c81aef89650
Pallavi <pallaviarshanapelli@gmail.com>
2026-07-25 01:03:26 +0530

    ui-polished improved

--- js/renderer.js
+++ js/renderer.js
@@ -10,7 +10,7 @@ function createGraphContainer() {
   return graphContainer;
 }
 
-const SCALE_FACTOR = 6;
+const SCALE_FACTOR = 12;
 function renderBars(randomArray, graphContainer) {
   const barsContainer = document.createElement("div");
   barsContainer.className = "bars-container";
